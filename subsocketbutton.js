69f352a9263e71d65c6205b6896cafc30a30ebb4
Thimows <62159496+Thimows@users.noreply.github.com>
2021-05-08 21:36:16 +0200

    Update subsocketbutton.js

--- subsocketbutton.js
+++ subsocketbutton.js
@@ -1,38 +1,39 @@
 /**
  * Copyright SubSocket.io
- * Version 0.1.4
+ * Version 0.1.5
  */
 
-  function SubSocketButton(checkoutID, versionTest = false, config, elementID) {
+function SubSocketButton(checkoutID, versionTest = false, config, elementID) {
 
-    if (document.querySelector('#'+elementID+' .paypal-buttons') !== null) {
+  if (document.querySelector('#' + elementID + ' .paypal-buttons') !== null) {
 
-      document.querySelector('#'+elementID+' .paypal-buttons').innerHTML = ''
-    }
+    document.querySelector('#' + elementID + ' .paypal-buttons').innerHTML = ''
+  }
 
-    if (versionTest) {
-      var verstionTest = 'version-test'
-      }else {
-        var verstionTest = 'version-live'
-        }
+  var versionTest
+  if (versionTest) {
+    verstionTest = 'version-test'
+  } else {
+    verstionTest = 'version-live'
+  }
 
-    //Recieve checkout details from SubSocket
-    fetch(
+  //Recieve checkout details from SubSocket
+  fetch(
       `${"https://www.subsocket.io/"+verstionTest+"/api/1.1/obj/checkout/" + checkoutID}`
     )
-      .then(function (response) {
+    .then(function(response) {
       // The API call was successful!
       return response.json();
     })
-      .then(function (data) {
+    .then(function(data) {
       // This is the JSON from our response
-      if (document.querySelector('#'+elementID+' .paypal-buttons') == null) {
-        setTimeout(function () {
+      if (document.querySelector('#' + elementID + ' .paypal-buttons') == null) {
+        setTimeout(function() {
           renderButton(data.response);
         }, 1)
       }
     })
-      .catch(function (err) {
+    .catch(function(err) {
       // There was an error
       console.warn(
         "Something went wrong with retrieving the Checkout details from SubSocket.",
@@ -41,82 +42,90 @@
     });
 
 
-    const renderButton = (response) => {
-      paypal
-        .Buttons({
+  const renderButton = (response) => {
+    paypal
+      .Buttons({
 
         style: config.style,
 
-        createSubscription: function (data, actions) {
+        createSubscription: function(data, actions) {
           return actions.subscription.create({
             plan_id: response["Plan ID"]
           });
         },
 
-        onApprove: function (data, actions) {
+        onApprove: function(data, actions) {
           subscriptionSuccesful(response, data.subscriptionID)
         },
 
-        onError: function (err) {
+        onError: function(err) {
           // For example, redirect to a specific error page
           console.log(err)
         }
       })
-        .render("#" + elementID);
-    };
+      .render("#" + elementID);
+  };
 
-    const subscriptionSuccesful = (response, subscriptionID) => {
+  const subscriptionSuccesful = (response, subscriptionID) => {
 
-      createSubscription(response, subscriptionID)
-    }
+    createSubscription(response, subscriptionID)
+  }
 
-    const createSubscription = (response, subscriptionID) => {
+  const createSubscription = (response, subscriptionID) => {
 
-      var body = JSON.stringify({
-        "Checkout": checkoutID,
-        "Subscription ID": subscriptionID,
-        "Application": response['Application'],
-        "Sandbox mode": response['Sandbox mode'],
-        "Status": 'success',
-        //"Subscription detail (object)": data,
-        "User name": "Click to load",
-        "User email": "Click to load",
-        "Plan ID": response['Plan ID'],
-        "Status (subscription object)": 'SUCCESS',
-        "Log" : "Created using SubSocket button"
-      })
+    var body = JSON.stringify({
+      "Checkout": checkoutID,
+      "Subscription ID": subscriptionID,
+      "Application": response.Application,
+      "Sandbox mode": response['Sandbox mode'],
+      "Status": 'success',
+      //"Subscription detail (object)": data,
+      "User name": "[Created via SubSocket button] Click to load user information",
+      "User email": "[Created via SubSocket button] Click to load user information",
+      "Plan ID": response['Plan ID'],
+      "Status (subscription object)": 'SUCCESS',
+      "Log": "Created using SubSocket button"
+    })
 
-      //POST API call to create subscription inside SubSocket
-      fetch(`${"https://www.subsocket.io/"+verstionTest+"/api/1.1/obj/Subscriptions"}`, {
+    //POST API call to create subscription inside SubSocket
+    fetch(`${"https://www.subsocket.io/"+verstionTest+"/api/1.1/obj/Subscriptions"}`, {
         method: 'POST',
         body: body, // The data
         headers: {
           'Content-type': 'application/json' // The type of data you're sending
         }
-      })     
-        .then(function (result) {
+      })
+
+      .then(function(result) {
         // The API call was successful!
         console.log("Subscription successfully created in SubSocket")
-
-        //Redirect user to success URL
-        var successURL = response['Success URL']
-        console.log(successURL)
-        var URLParameters = successURL.match('(\?|\&)([^=]+)\=([^&]+)')      
-
-        if (URLParameters.length == 0) {
-          var finalURL = successURL + '?subscription_id=' + subscriptionID + '&checkout_id=' + checkoutID
-          } else {
-            var finalURL = successURL + '&subscription_id=' + subscriptionID + '&checkout_id=' + checkoutID
-            }
-        console.log('Redirecting to', finalURL)
-        window.location.href(finalURL);
+        actionSuccessful(response, subscriptionID)
       })
-        .catch(function (err) {
+
+      .catch(function(err) {
         // There was an error
         console.warn(
           "Something went wrong with creating the subsciption in SubSocket.",
           err
         );
       });
+
+
+    const actionSuccessful = (response, subscriptionID) => {
+      //Redirect user to success URL
+      var successURL = response['Succes URL']
+
+      var url = new URL(successURL);
+      var lengthURLParameters = Array.from(url.searchParams).length
+
+      var finalURL
+      if (lengthURLParameters == 0) {
+        finalURL = successURL + '?subscription_id=' + subscriptionID + '&checkout_id=' + checkoutID
+      } else if (lengthURLParameters > 0){
+        finalURL = successURL + '&subscription_id=' + subscriptionID + '&checkout_id=' + checkoutID
+      }
+      console.log('Redirecting to', finalURL)
+      window.location.href = finalURL;
     }
-    }
+  }
+}
