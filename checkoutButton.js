740c8ba29bad56c01e1286a738fd413eaee2c5bb
Thimows <62159496+Thimows@users.noreply.github.com>
2021-05-14 12:52:35 +0200

    Update checkoutButton.js

--- checkoutButton.js
+++ checkoutButton.js
@@ -1,9 +1,14 @@
 /**
  * Copyright SubSocket.io
- * Version 1.1.7
+ * Version 1.1.8
  */
 
-function SubSocketButton(checkoutID, versionTest = false, config, elementID, application) {
+function SubSocketButton(checkoutID, versionTest = false, config, elementID, application, limit) {
+
+var sheet = document.createElement('style');
+sheet.innerHTML = ".subsocket_warning_img { width: 100% }";
+document.body.appendChild(sheet); // append in body
+
 
   if (document.querySelector('#' + elementID + ' .paypal-buttons') !== null) {
 
@@ -16,7 +21,72 @@ function SubSocketButton(checkoutID, versionTest = false, config, elementID, app
   } else {
     versionTest = 'version-live'
   }
-  
+
+  var today = new Date()
+  var priorDate = new Date().setDate(today.getDate() - 30)
+  var totalAmount = 0
+
+  //Retrieve total count of subscription/transactions last 30 days
+  //Retrieve total subscriptions
+  fetch(
+      `${"https://www.subsocket.io/"+versionTest+"/api/1.1/obj/subscriptions?constraints=%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22key%22%3A%20%22Created%20Date%22%2C%0A%20%20%20%20%20%20%20%20%22constraint_type%22%3A%20%22greater%20than%22%2C%0A%20%20%20%20%20%20%20%20%22value%22%3A%20%22"+priorDate+"%22%0A%20%20%20%20%7D%2C%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22key%22%3A%20%22Application%22%2C%0A%20%20%20%20%20%20%20%20%22constraint_type%22%3A%20%22equals%22%2C%0A%20%20%20%20%20%20%20%20%22value%22%3A%20%22"+application+"%22%0A%20%20%20%20%7D%0A%5D"}`
+    )
+    .then(function(response) {
+      // The API call was successful!
+      return response.json();
+    })
+    .then(function(data) {
+      // This is the JSON from our response
+      totalAmount += data.response.results.length
+      console.log(totalAmount)
+      amountTransactions()
+    })
+    .catch(function(err) {
+      // There was an error
+      console.warn(
+        "Something went wrong with retrieving the Subscription amount from SubSocket.",
+        err
+      );
+    });
+
+
+  const amountTransactions = () => {
+
+    //Retrieve total transacions
+    fetch(
+        `${"https://www.subsocket.io/"+versionTest+"/api/1.1/obj/transactions?constraints=%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22key%22%3A%20%22Created%20Date%22%2C%0A%20%20%20%20%20%20%20%20%22constraint_type%22%3A%20%22greater%20than%22%2C%0A%20%20%20%20%20%20%20%20%22value%22%3A%20%22"+priorDate+"%22%0A%20%20%20%20%7D%2C%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%22key%22%3A%20%22Application%22%2C%0A%20%20%20%20%20%20%20%20%22constraint_type%22%3A%20%22equals%22%2C%0A%20%20%20%20%20%20%20%20%22value%22%3A%20%22"+application+"%22%0A%20%20%20%20%7D%0A%5D"}`
+      )
+      .then(function(response) {
+        // The API call was successful!
+        return response.json();
+      })
+      .then(function(data) {
+        // This is the JSON from our response
+        totalAmount += data.response.results.length
+        console.log(totalAmount)
+        if (totalAmount > limit && window.location.origin !== 'https://subsocket.io') {
+	
+          document.querySelector('#' + elementID + ' .paypal-buttons').style.display = 'none';
+          var img = document.createElement('img');
+          img.setAttribute("class", "subsocket_warning_img")
+          img.src = 'https://s3.amazonaws.com/appforest_uf/f1620944861081x858362906518816800/SubSocket%20plan%20limit%20reached.png';
+          var element = document.querySelector('#' + elementID)
+          element.appendChild(img);
+
+          const redirectToSubSocket = () => window.location.href = "https://subsocket.io"
+          img.onclick = redirectToSubSocket
+
+        }
+      })
+      .catch(function(err) {
+        // There was an error
+        console.warn(
+          "Something went wrong with retrieving the Transaction amount from SubSocket.",
+          err
+        );
+      });
+  }
+
 
   //Recieve checkout details from SubSocket
   fetch(
@@ -67,7 +137,6 @@ function SubSocketButton(checkoutID, versionTest = false, config, elementID, app
       })
       .render("#" + elementID);
   };
-  
 
   const createSubscription = (response, subscriptionID) => {
 
@@ -98,6 +167,7 @@ function SubSocketButton(checkoutID, versionTest = false, config, elementID, app
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
   }
+
 
 
   const actionSuccessful = (response, subscriptionID) => {
